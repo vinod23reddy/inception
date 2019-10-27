4c9e90a760147f3027d4d5b4b2e3340021fa21b9
vinod23reddy <vinod23reddy@gmail.com>
2019-10-27 21:14:37 +0100

    Add connection checker to external recommender settings
    Answer: Added a connection checker button next to the recommender settings for the remote URL.

--- inception-imls-external/src/main/java/de/tudarmstadt/ukp/inception/recommendation/imls/external/ExternalRecommenderTraitsEditor.java
+++ inception-imls-external/src/main/java/de/tudarmstadt/ukp/inception/recommendation/imls/external/ExternalRecommenderTraitsEditor.java
@@ -17,24 +17,35 @@
  */
 package de.tudarmstadt.ukp.inception.recommendation.imls.external;
 
-import static de.tudarmstadt.ukp.clarin.webanno.support.lambda.LambdaBehavior.visibleWhen;
+import java.awt.Button;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
+import org.apache.wicket.feedback.FeedbackMessage;
+import org.apache.wicket.feedback.IFeedbackMessageFilter;
 import org.apache.wicket.markup.html.form.CheckBox;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.form.validation.IFormValidator;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.validation.IValidatable;
+import org.apache.wicket.validation.IValidator;
+import org.apache.wicket.validation.ValidationError;
 import org.apache.wicket.validation.validator.UrlValidator;
 
-import de.tudarmstadt.ukp.clarin.webanno.support.lambda.LambdaAjaxFormComponentUpdatingBehavior;
 import de.tudarmstadt.ukp.inception.recommendation.api.model.Recommender;
-import de.tudarmstadt.ukp.inception.recommendation.api.recommender.DefaultTrainableRecommenderTraitsEditor;
 import de.tudarmstadt.ukp.inception.recommendation.api.recommender.RecommendationEngineFactory;
 
 public class ExternalRecommenderTraitsEditor
-    extends DefaultTrainableRecommenderTraitsEditor
+    extends Panel
 {
     private static final long serialVersionUID = 1677442652521110324L;
 
@@ -67,15 +78,85 @@ public class ExternalRecommenderTraitsEditor
         remoteUrl.setRequired(true);
         remoteUrl.add(new UrlValidator());
         form.add(remoteUrl);
+        
+        Button applyButton = new Button("check") {
+    		private static final long serialVersionUID = 1L;
 
+    		public void onSubmit() {
+    			remoteUrl.add(new ExternalConnectionChecker());
+    		}
+
+    	};
+    	
+    	form.add((IFormValidator) applyButton);
+        
+        add(new FeedbackPanel("feedbackMessage", 
+        		new ExactErrorLevelFilter(FeedbackMessage.ERROR)).setOutputMarkupId(true));
+        add(new FeedbackPanel("succesMessage", 
+        		new ExactErrorLevelFilter(FeedbackMessage.SUCCESS)).setOutputMarkupId(true));
+        
         CheckBox trainable = new CheckBox("trainable");
-        trainable.setOutputMarkupId(true);
-        trainable.add(new LambdaAjaxFormComponentUpdatingBehavior("change", _target -> 
-                _target.add(getTrainingStatesChoice())));
         form.add(trainable);
-        
-        getTrainingStatesChoice().add(visibleWhen(() -> trainable.getModelObject() == true));
-        
+
         add(form);
     }
+    
+    
+    
+    
+    class ExactErrorLevelFilter implements IFeedbackMessageFilter{
+    	private int errorLevel;
+
+		public ExactErrorLevelFilter(int errorLevel){
+			this.errorLevel = errorLevel;
+		}
+
+		@Override
+		public boolean accept(FeedbackMessage message) {
+			return message.getLevel() == errorLevel;
+		}
+    }
+
+    class ExternalConnectionChecker implements IValidator<String>  {
+
+		@Override
+		public void validate(IValidatable<String> validatable){
+			String urlRecieved = validatable.getValue();
+
+			HttpURLConnection.setFollowRedirects(false);
+		    HttpURLConnection con = null;
+			try {
+				con = (HttpURLConnection) new URL(urlRecieved).openConnection();
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		    try {
+				con.setRequestMethod("HEAD");
+			} catch (ProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		    try {
+				if(con.getResponseCode() != 200){
+					ValidationError error = new ValidationError(this);
+					error.setVariable("statusCode", 
+							+con.getResponseCode());
+					validatable.error(error);
+				}
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+		}
+
+    }	
+    
+    
+    
 }
